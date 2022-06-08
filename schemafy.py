ae607869723c5701b8de708382bb9293f26958c1
Michael Shoemaker <mikeshoemaker2000@yahoo.com>
2022-06-08 16:01:01 -0500

    Added coding to add commas
    Add commas to the end of every column definition except for the last

--- schemafy.py
+++ schemafy.py
@@ -1,4 +1,5 @@
 import re
+from pyspark.sql import types
 raw = 'StructType(List(StructField(hvfhs_license_num,StringType,true),StructField(dispatching_base_num,StringType,true),StructField(pickup_datetime,StringType,true),StructField(dropoff_datetime,StringType,true),StructField(PULocationID,LongType,true),StructField(DOLocationID,LongType,true),StructField(SR_Flag,DoubleType,true)))'
 
 def make_schema(raw):
@@ -9,8 +10,16 @@ def make_schema(raw):
         fields.append(raw[i[0]+1:i[1]])
 
     schema = "types.StructType[\n"
+
+    ending = '\n'
+    commas = len(fields) -1
+    
     for f in fields:
         n = f.split(",")
-        schema = schema+"\ttypes.StructField(\""+n[0]+"\", types."+n[1]+"(), "+n[2].capitalize()+")\n"
+        schema = schema+"\ttypes.StructField(\""+n[0]+"\", types."+n[1]+"(), "+n[2].capitalize()+")"
+        if commas > 0:
+            schema = schema + ',' + ending
+            commas -= 1
+    schema = schema + ending
     schema = schema +"])"
-    return schema
+    print(schema)
